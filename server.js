786222ab64f7d685834fcf21e3616488a64a2598
axel <axel.valentinbvs@yahoo.com>
2023-04-24 09:36:05 +0000

    ESlint

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-import express from "express";
-import routes from "./routes/index.js";
+import express from 'express';
+
+const routes = require('./routes/index');
+
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use("/", routes);
+app.use('/', routes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
